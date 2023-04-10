ef9050f4b2d0ea981b3b73a2f74abd33639a95f2
mdyasin111 <mdyasin7636@gmail.com>
2023-04-10 22:47:36 +0600

    banner part done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between px-20 py-2 bg-indigo-50">
+    <div className="flex items-center justify-between bg-indigo-50 p-10">
       <p className="text-2xl font-bold">Rapid Talent</p>
-      <div className="flex justify-center flex-1 space-x-4 text-base font-medium">
-        <Link to="/" className="text-gray-700 hover:text-gray-900">
+      <div className="flex justify-center flex-1 space-x-4 font-medium">
+        <Link to="/" className="text-zinc-500 hover:text-gray-900">
           Home
         </Link>
-        <Link to="/statistics" className="text-gray-700 hover:text-gray-900">
+        <Link to="/statistics" className="text-zinc-500 hover:text-gray-900">
           Statistics
         </Link>
-        <Link to="/appliedJobs" className="text-gray-700 hover:text-gray-900">
+        <Link to="/appliedJobs" className="text-zinc-500 hover:text-gray-900">
           Applied Jobs
         </Link>
-        <Link to="/blog" className="text-gray-700 hover:text-gray-900">
+        <Link to="/blog" className="text-zinc-500 hover:text-gray-900">
           Blog
         </Link>
       </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
+
+
 
 const Home = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="banner-container flex flex-col md:flex-row items-center justify-center bg-indigo-50 px-16">
+        <div className="w-full md:w-1/2 px-4 py-8 text-center md:text-left">
+          <h1 className="text-6xl font-bold mb-4">
+            One Step <br /> Closer To Your <br />{" "}
+            <span className="text-indigo-500">Dream Job</span>{" "}
+          </h1>
+          <p className="mb-4 text-zinc-600 font-medium">
+            Explore thousands of job opportunities with all the <br />{" "}
+            information you need. Its your future. Come find it. <br /> Manage
+            all your job application from start to finish.
+          </p>
+          <button className="px-4 py-2 font-bold text-white bg-indigo-500 rounded-md hover:bg-indigo-600">
+            Get Started
+          </button>
         </div>
-    );
+        <div className="w-full md:w-1/2 mb-8 md:mb-0">
+          <img
+            src="https://i.ibb.co/99Cp5q7/person.png"
+            alt="person"
+            className="mx-auto md:ml-auto md:mr-0"
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+        <h1>Job Category List</h1>
+        <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+        </div>
+        <div className="bg-indigo-50 ">
+            <img src="https://i.ibb.co/W34sKKB/social-media-1.png" alt="" />
+            <h3>Account & Finance</h3>
+            <p>300 Jobs Available</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
